4c08e239088f97ed7f35d8c18169ddcf30fedfdf
goddoro <dongsang1222@daum.net>
2023-02-14 01:58:36 +0900

    feat: extension 함수들 추가

--- core/src/main/java/doro/android/core/util/extension.kt
+++ core/src/main/java/doro/android/core/util/extension.kt
@@ -1,6 +1,29 @@
 package doro.android.core.util
 
+import android.app.Activity
+import android.content.Intent
+
+inline fun Activity.startActivityWithAnimation(
+    withFinish: Boolean = false,
+    intentBuilder: () -> Intent,
+) {
+    startActivity(intentBuilder())
+    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+    if (withFinish) {
+        finish()
+    }
+}
+
 @Suppress("UNCHECKED_CAST")
 fun HashMap<String, out Any?>.filterValueNotNull(): HashMap<String, Any> {
     return this.filter { it.value != null } as HashMap<String, Any>
+}
+
+inline fun <T> T.runIf(
+    condition: Boolean,
+    run: T.() -> T,
+) = if (condition) {
+    run()
+} else {
+    this
 }
